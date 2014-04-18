85032fbc229f30f0e95e7f80451583e09cd1277f
chanpod <chanpod36@hotmail.com>
2014-04-17 21:15:06 -0500

    File transfer framework

--- server.c
+++ server.c
@@ -1,75 +1,109 @@
-#include "packet.h"
-#include "gremlin.h"
-#include "checksum.h"
-#include "debug.h"
+/*
+** listener.c -- a datagram sockets "server" demo
+*/
 
-#include <string.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
+#include "server.h"
 
-#define SERVERPORT "10030"
-int main(int argc, char *argv[]){
-	int sockfd;
-	    struct addrinfo hints, *servinfo, *p;
-	    int rv;
-	    int numbytes;
 
-	    if (argc != 3) {
-	        fprintf(stderr,"usage: talker hostname message\n");
-	        exit(1);
-	    }
+int main(void)
+{
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+    struct sockaddr_storage their_addr;
+    char *buf;
+    socklen_t addr_len;
+    char s[INET6_ADDRSTRLEN];
+    FILE *fr;
 
-	    memset(&hints, 0, sizeof hints);
-	    hints.ai_family = AF_UNSPEC;
-	    hints.ai_socktype = SOCK_DGRAM;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC; // set to AF_INET to force IPv4
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
 
-	    if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) {
-	        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-	        return 1;
-	    }
+    if ((rv = getaddrinfo(NULL, MYPORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
 
-	    // loop through all the results and make a socket
-	    for(p = servinfo; p != NULL; p = p->ai_next) {
-	        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-	                p->ai_protocol)) == -1) {
-	            perror("talker: socket");
-	            continue;
-	        }
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("listener: socket");
+            continue;
+        }
 
-	        break;
-	    }
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("listener: bind");
+            continue;
+        }
 
-	    if (p == NULL) {
-	        fprintf(stderr, "talker: failed to bind socket\n");
-	        return 2;
-	    }
+        break;
+    }
 
-	    if ((numbytes = sendto(sockfd, argv[2], strlen(argv[2]), 0,
-	             p->ai_addr, p->ai_addrlen)) == -1) {
-	        perror("talker: sendto");
-	        exit(1);
-	    }
+    if (p == NULL) {
+        fprintf(stderr, "listener: failed to bind socket\n");
+        return 2;
+    }
 
-	    freeaddrinfo(servinfo);
+    freeaddrinfo(servinfo);
 
-	    printf("talker: sent %d bytes to %s\n", numbytes, argv[1]);
-	    close(sockfd);
+    printf("listener: waiting to recvfrom...\n");
 
-	    return 0;
-	}
+    addr_len = sizeof their_addr;
+
+    while(1){
+    	char *fileName;
+		if ((numbytes = recvfrom(sockfd, buf, strlen(buf) , 0,
+			(struct sockaddr *)&their_addr, &addr_len)) == -1) {
+			perror("recvfrom");
+			exit(1);
+		}
+
+		//fileName = checkForGet(buf);
+		strcpy(fileName, checkForGet(buf));
+
+		fr = fopen(fileName, "r");
+		if (fr == NULL){
+			replyWithInvalidFile(p, sockfd);
+		}
+
+    }
+
+    close(sockfd);
+
+    return 0;
 }
 
-int listen(char* fileName){
-   printf("Listen is not yet implemented.\n"); 
-   strcpy(fileName, "test.txt");
-   return 0;
+void replyWithInvalidFile(struct addrinfo *p, int sockfd){
+
+	int numBytes;
+	char message[8] = "invalid";
+	struct sockaddr_storage their_addr;
+				socklen_t addr_len;
+
+	if ((numBytes = sendto(sockfd, message, strlen(message), 0,
+									(struct sockaddr *)&their_addr, addr_len)) == -1) {
+										perror("talker: sendto");
+							}
+}
+
+char* checkForGet(char *packetIn){
+	char *splitString;
+	char *cmp;
+	int valid = 0;
+	char COMMAND[3] = "GET";
+
+	splitString = strsep(&packetIn, ",");
+
+	sscanf(cmp, "%s", &packetIn[0]);
+
+	if(strcmp(packetIn,COMMAND)){
+		valid = 1;
+	}
+
+	return cmp;
 }
