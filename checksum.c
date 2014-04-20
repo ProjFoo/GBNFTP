01e94ed08da17b5a54763de15b78ce24e49bc2b1
benjwrdill <benjwrdill@gmail.com>
2014-04-20 17:02:41 -0500

    cleaned up old code

--- checksum.c
+++ checksum.c
@@ -14,22 +14,6 @@ unsigned char* checksum(char* message, unsigned char* myChecksum)
     MD5_Update(&mdContext, message, strlen(message));
     MD5_Final(myChecksum, &mdContext);
 
-
-	//int i = 0;
-//	for( i ; i <= strlen(message); i = i + 16)
-//	{
-//		if( i + 16 > strlen(message) ) {
-//			memcpy(currentSubString, &message[i], strlen( message ) - i );
-//			int k = strlen( message ) - i;
-//			for( k; k < 16; k++ ){
-//				currentSubString[k] = '\0';
-//			}
-//		}
-//        else{
-//		memcpy(currentSubString, &message[i], 16);
-//        }
-//
-//        mysum = (int)mysum + (int)currentSubString;
 #ifdef DEBUG
     printf("Checksum is: %s\n", myChecksum);
 #endif
