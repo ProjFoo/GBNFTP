92c282b89f4c2a6d6f09057328c76cf08ee3892f
benjwrdill <benjwrdill@gmail.com>
2014-04-22 15:20:17 -0500

    implementing gremlin; still needs delay code

--- gremlin.c
+++ gremlin.c
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-int gremlin(char* message, int damage, int lost, int delay, int delaytime) {
+int gremlin(char ** message, float damaged, float lost, float delay, int delaytime, unsigned int r) {
 
 #ifdef DEBUG
     printf("Message: %s\n"
@@ -13,7 +13,97 @@ int gremlin(char* message, int damage, int lost, int delay, int delaytime) {
            "Delay Time: %i ms"
            , message, damage, lost, delay, delaytime);
 #endif
-    printf("Gremlin functionality has yet to be implemented.\n");
 
-    return 1;
+	int returnNum;
+	int sizeOfPacket = strlen(*packet);
+	int s = r % 100;
+	int p_dam = damaged * 100,
+		p_lost = lost * 100,
+		p_delay = delay * 100;
+
+
+	if (r <= p_dam)
+	{
+
+		damage(packet);
+
+		returnNum = 1;
+	}
+	else if (r <= p_dam + p_lost)
+	{
+
+		returnNum = 0;
+
+	}
+	else if (r <= p_dam + p_lost + p_delay)
+	{
+	    delay(message, delaytime);
+	    returnNum = 2;
+	}
+	else {
+		returnNum = 1;
+	}
+
+	return returnNum;
+}
+
+char * damage(char **p)
+{
+
+	const int ONE = 7,
+			TWO = 2,
+			THREE = 1;
+
+	char *msg = (*p);
+	char corruptor = '0';
+	int r = rand() % 10;
+
+
+
+	if (r < ONE )
+	{
+
+		int s = rand() % sizeof(*p);
+		msg[s] = corruptor;
+
+	}
+
+	else if(r < (ONE + TWO) )
+	{
+
+		int s = rand() % 124;
+		msg[s] = corruptor;
+		s = rand() % 124;
+		while (msg[s] == corruptor)
+			{
+			s = rand() % 124;
+			msg[s] = corruptor;
+			s = rand() % 124;
+			}
+	}
+	else if (r < ONE + TWO + THREE)
+	{
+
+		int s = rand() % 124;
+		msg[s] = corruptor;
+
+		while (msg[s] == corruptor)
+			{
+				s = rand() % 124;
+				msg[s] = corruptor;
+				s = rand() % 124;
+			}
+		while (msg[s] == corruptor)
+			{
+				s = rand() % 124;
+				msg[s] = corruptor;
+				s = rand() % 124;
+			}
+	}
+
+	else
+	{
+
+	}
+	return *p;
 }
