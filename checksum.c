a3b308589e8d25dc0ad3065b4f43580a87cd96af
benjwrdill <benjwrdill@gmail.com>
2014-04-22 15:01:46 -0500

    sixteen bit checksum, not sixteen byte

--- checksum.c
+++ checksum.c
@@ -7,12 +7,36 @@
 
 unsigned char* checksum(char* message, unsigned char* myChecksum)
 {
+	
+	//if we're using a sixteen byte checksum, MD5 it.
+#ifdef SIXTEEN_BYTE_CHECKSUM
     MD5_CTX mdContext;
 
 
     MD5_Init (&mdContext);
     MD5_Update(&mdContext, message, strlen(message));
     MD5_Final(myChecksum, &mdContext);
+#else
+    int sum = 0;
+    char *mysum = malloc(20),
+
+    myChecksum[2] = '\0';
+	int i = 0;
+	for( i ; i <= strlen(message); i++)
+	{
+		sum = sum + (int)message[i];
+	}
+
+    sprintf(mysum, "%d", sum);
+    i = 0;
+
+    myChecksum[0] = mysum[strlen(mysum) - 2];
+    myChecksum[1] = mysum[strlen(mysum) - 1];
+    myChecksum[2] = '\0';
+
+
+    free(mysum);
+#endif
 
 #ifdef DEBUG
     printf("Checksum is: %s\n", myChecksum);
